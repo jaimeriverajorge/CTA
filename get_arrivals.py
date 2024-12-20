189f1b0ece72a96968e8b33554d8996be748585e
jaimeriverajorge <70618882+jaimeriverajorge@users.noreply.github.com>
2024-12-19 20:02:53 -0600

    added train info

--- get_arrivals.py
+++ get_arrivals.py
@@ -1,4 +1,6 @@
 import requests
+import json 
+
 # this script uses the python requests package and the CTA train tracker API
 # to send a request a sample request taken from the train tracker API docs:
 # https://www.transitchicago.com/developers/ttdocs/#_Toc296199906
@@ -7,7 +9,7 @@ import requests
 # request:
 # http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=&max=1& mapid=40360&outputType=JSON
 
-key = "971b7cc22ddf4804902cc49d2c8b6063"
+key = ""
 
 # Define the URL with query parameters
 url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx"
@@ -26,7 +28,54 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     # Parse the JSON response
     data = response.json()
-    # Print or process the JSON data
-    print(data)
+
+    # Print the JSON data
+    #print("This is the parsed response from requests package")
+   # print(data)
+
 else:
     print(f"Error: {response.status_code}")
+
+#pass in json parsed response
+def format(data):
+
+    #from the returned data, grab the main ETA dictionary:
+    eta = data['ctatt']['eta']
+
+    #grab first train
+    train1 = eta[0]
+
+    #print the key value pairs with a new line in between
+    #for key in train1:
+     #   print(key, (train1[key]))
+
+    for train in eta:
+
+    
+        #get stop name
+        stop = train['staNm']
+
+        #get the destination
+        direction = train['stpDe']
+        
+        #get the arrival time of the next train
+        arrivalTime = train['arrT']
+
+        #split by date and time
+        date, time = arrivalTime.split('T')
+
+        print("Station:")
+        print(stop)
+        print(direction)
+
+        print("Arrival:")
+        print("Date:", date)
+        
+        print("Time:", time)
+
+format(data)
+
+#  return {
+#        'statusCode': 200,
+#        'body':'Hello from Lambda!'
+#    }
